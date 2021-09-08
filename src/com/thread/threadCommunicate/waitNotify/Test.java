d886965021946332913ad83055aa05c402fae145
wzy <wangzy0327@qq.com>
2021-09-08 18:13:02 +0800

    add chapter03 thread switch process contents

---
+++ src/com/thread/threadCommunicate/waitNotify/Test.java
@@ -0,0 +1,27 @@
+package com.thread.threadCommunicate.waitNotify;
+
+/**
+ *
+ * 方法notify()执行后不释放锁，必须执行完notify()方法所在的同步synchronized代码块后才释放锁
+ */
+public class Test {
+    public static void main(String[] args) {
+        Object lock = new Object();
+        ThreadA a = new ThreadA(lock);
+        a.start();
+        NotifyThread notifyThread = new NotifyThread(lock);
+        notifyThread.start();
+        SynNotifyMethodThread c = new SynNotifyMethodThread(lock);
+        c.start();
+    }
+
+    /**
+     *
+     * begin wait() ThreadName = Thread-0
+     * begin notify() ThreadName = Thread-1 time = 1631095757083
+     *   end notify() ThreadName = Thread-1 time = 1631095762093
+     *  end wait() ThreadName = Thread-0
+     * begin notify() ThreadName = Thread-2 time = 1631095762093
+     *   end notify() ThreadName = Thread-2 time = 1631095767095
+     */
+}
